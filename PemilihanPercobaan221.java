340ed21a1647b40c9d988dd262a649988cc47ab8
Nurwahibzy <nurwahibzakkiy@gmail.com>
2023-10-05 14:12:34 +0700

    Membenahi statement yang berantakan

--- PemilihanPercobaan221.java
+++ PemilihanPercobaan221.java
@@ -2,31 +2,57 @@ import java.util.Scanner;
 
 public class PemilihanPercobaan221 {
     public static void main(String[] args) {
-     Scanner input21 = new Scanner(System.in);
-     System.out.print("Nilai uas : ");
-     float uas = input21.nextFloat();
-     System.out.print("Nilai uts : ");
-     float uts = input21.nextFloat();
-     System.out.print("Nilai kuiz : ");
-     float kuiz = input21.nextFloat();
-     System.out.print("Nilai tugas : ");
-     float tugas = input21.nextFloat();
-     float total = (uas * 0.4F) + (uts * 0.3F) + (kuiz * 0.1F) + (tugas * 0.2F);
-     
-     if (total > 80 && total <=100){
-        System.out.print("Nilai angka = " + total + ", Nilai huruf = A, " + "Nilai setara = 4, " + "Kualifikasi = Sangat Baik.");
-     }else if (total > 73 && total <=80){
-        System.out.print("Nilai angka = " + total + ", Nilai huruf = A, " + "Nilai setara = 4, " + "Kualifikasi = Sangat Baik.");
-     }else if (total > 65 && total <=73){
-        System.out.print("Nilai angka = " + total + ", Nilai huruf = A, " + "Nilai setara = 4, " + "Kualifikasi = Sangat Baik.");
-     }else if (total > 60 && total <=65){
-        System.out.print("Nilai angka = " + total + ", Nilai huruf = A, " + "Nilai setara = 4, " + "Kualifikasi = Sangat Baik.");
-     }else if (total > 50 && total <=60){
-        System.out.print("Nilai angka = " + total + ", Nilai huruf = A, " + "Nilai setara = 4, " + "Kualifikasi = Sangat Baik.");
-     }else if (total > 39 && total <=50){
-        System.out.print("Nilai angka = " + total + ", Nilai huruf = A, " + "Nilai setara = 4, " + "Kualifikasi = Sangat Baik.");
-     }else if (total < 39 ){
-        System.out.print("Nilai angka = " + total + ", Nilai huruf = A, " + "Nilai setara = 4, " + "Kualifikasi = Sangat Baik.");
-     }
+        Scanner input21 = new Scanner(System.in);
+        System.out.print("Nilai uas : ");
+        float uas = input21.nextFloat();
+        System.out.print("Nilai uts : ");
+        float uts = input21.nextFloat();
+        System.out.print("Nilai kuiz : ");
+        float kuiz = input21.nextFloat();
+        System.out.print("Nilai tugas : ");
+        float tugas = input21.nextFloat();
+        float total = (uas * 0.4F) + (uts * 0.3F) + (kuiz * 0.1F) + (tugas * 0.2F);
+        // membuat variabel untuk nilai huruf, nilai setara, dan kualifikasi
+        String nilaiHuruf;
+        double nilaiSetara;
+        String kualifikasi;
+        //struktur pemilihan
+        if (total > 80 && total <= 100) {
+            nilaiHuruf = "A";
+            nilaiSetara = 4;
+            kualifikasi = "Sangat Baik";
+        } else if (total > 73 && total <= 80) {
+            nilaiHuruf = "B+";
+            nilaiSetara = 3.5;
+            kualifikasi = "Lebih dari Baik";
+        } else if (total > 65 && total <= 73) {
+            nilaiHuruf = "B";
+            nilaiSetara = 3;
+            kualifikasi = "Baik";
+        } else if (total > 60 && total <= 65) {
+            nilaiHuruf = "C+" ;
+            nilaiSetara = 2.5;
+            kualifikasi = "Lebih dari cukup";
+        } else if (total > 50 && total <= 60) {
+            nilaiHuruf = "C";
+            nilaiSetara = 2;
+            kualifikasi = "Cukup";
+        } else if (total > 39 && total <= 50) {
+            nilaiHuruf = "D";
+            nilaiSetara = 1;
+            kualifikasi = "Kurang";
+        } else if (total < 39) {
+            nilaiHuruf = "E" ;
+            nilaiSetara = 0;
+            kualifikasi = "Gagal";
+        }else{
+            nilaiHuruf = "error";
+            nilaiSetara = 00;
+            kualifikasi = "error";
+        }
+        System.out.println("Nilai angka : " + total);
+        System.out.println("Nilai huruf : " + nilaiHuruf);
+        System.out.println("Nilai setara : " + nilaiSetara);
+        System.out.println("Kualifikasi : " + kualifikasi);
     }
 }
---
+++ PemilihanPercobaan321.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class PemilihanPercobaan321 {
+    public static void main(String[] args) {
+        double angka1, angka2, hasil;
+        char operator;
+        Scanner input21 = new Scanner(System.in);
+        System.out.print("Masukkan angka pertama");
+        angka1 = input21.nextDouble();
+        System.out.print("Masukkan angka pertama");
+        angka2 = input21.nextDouble();
+        System.out.print("Masukkan operator (+ - * /): ");
+        operator = input21.next().charAt(0);
+
+    }
+}
