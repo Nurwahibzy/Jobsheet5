2db70d5b80f3a28b1f21308372917bfbb734825e
Nurwahibzy <nurwahibzakkiy@gmail.com>
2023-10-05 13:25:32 +0700

    Memodifikasi  program diatas sehingga dapat menampilkan nilai huruf

--- PemilihanPercobaan221.java
+++ PemilihanPercobaan221.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class PemilihanPercobaan221 {
     public static void main(String[] args) {
      Scanner input21 = new Scanner(System.in);
@@ -11,7 +12,21 @@ public class PemilihanPercobaan221 {
      System.out.print("Nilai tugas : ");
      float tugas = input21.nextFloat();
      float total = (uas * 0.4F) + (uts * 0.3F) + (kuiz * 0.1F) + (tugas * 0.2F);
-     String message = total < 65 ? "Remidi" : "Tidak remidi";
-     System.out.println("Nilai akhir = " + total + " sehingga " + message);
+     
+     if (total > 80 && total <=100){
+        System.out.print("Nilai angka = " + total + ", Nilai huruf = A, " + "Nilai setara = 4, " + "Kualifikasi = Sangat Baik.");
+     }else if (total > 73 && total <=80){
+        System.out.print("Nilai angka = " + total + ", Nilai huruf = A, " + "Nilai setara = 4, " + "Kualifikasi = Sangat Baik.");
+     }else if (total > 65 && total <=73){
+        System.out.print("Nilai angka = " + total + ", Nilai huruf = A, " + "Nilai setara = 4, " + "Kualifikasi = Sangat Baik.");
+     }else if (total > 60 && total <=65){
+        System.out.print("Nilai angka = " + total + ", Nilai huruf = A, " + "Nilai setara = 4, " + "Kualifikasi = Sangat Baik.");
+     }else if (total > 50 && total <=60){
+        System.out.print("Nilai angka = " + total + ", Nilai huruf = A, " + "Nilai setara = 4, " + "Kualifikasi = Sangat Baik.");
+     }else if (total > 39 && total <=50){
+        System.out.print("Nilai angka = " + total + ", Nilai huruf = A, " + "Nilai setara = 4, " + "Kualifikasi = Sangat Baik.");
+     }else if (total < 39 ){
+        System.out.print("Nilai angka = " + total + ", Nilai huruf = A, " + "Nilai setara = 4, " + "Kualifikasi = Sangat Baik.");
+     }
     }
 }
